3a0a2a75d12756df3ccb9d0cc386add9bef24f33
EstebanBra <Esteban.bravo2101@alumnos.ubiobio.cl>
2024-07-07 17:02:10 -0400

    Se agregaron las validaciones para los roles user y emprendedor

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -28,6 +28,55 @@ async function isAdmin(req, res, next) {
     return res.status(500).json({ message: 'Error interno del servidor' });
   }
 }
-// Se debe crear una funcion de validacion para cada rol, el admin ya esta creado, falta par el user y emprendedor
+// Funcion para validar el rol de user
+async function isUser(req, res, next) {
+  try {
+    // Verifica si hay un usuario autenticado en la sesión
+    if (!req.session.user) {
+      return res.status(401).json({ message: 'No estás autenticado' });
+    }
+    
+    // Obtiene el rol del usuario de la sesión
+    const userRole = req.session.user.rolName;
 
-export { isAdmin };
+    // Verifica si el usuario tiene el rol de usuario
+    if (userRole === 'usuario') {
+      // El usuario tiene el rol adecuado, continua con la siguiente función de middleware
+      next();
+      return;
+    } else {
+      // El usuario no tiene el rol adecuado, devuelve un error de acceso denegado
+      return res.status(403).json({ message: 'No tienes permisos para acceder a este recurso' });
+    }
+  } catch (error) {
+    console.log("Error en auth.middleware.js -> isUser(): ", error);
+    return res.status(500).json({ message: 'Error interno del servidor' });
+  }
+}
+
+// Funcion para validar el rol de emprendedor
+async function isEmprendedor(req, res, next) {
+  try {
+    // Verifica si hay un usuario autenticado en la sesión
+    if (!req.session.user) {
+      return res.status(401).json({ message: 'No estás autenticado' });
+    }
+    
+    // Obtiene el rol del usuario de la sesión
+    const userRole = req.session.user.rolName;
+
+    // Verifica si el usuario tiene el rol de emprendedor
+    if (userRole === 'emprendedor') {
+      // El usuario tiene el rol adecuado, continua con la siguiente función de middleware
+      next();
+      return;
+    } else {
+      // El usuario no tiene el rol adecuado, devuelve un error de acceso denegado
+      return res.status(403).json({ message: 'No tienes permisos para acceder a este recurso' });
+    }
+  } catch (error) {
+    console.log("Error en auth.middleware.js -> isEmprendedor(): ", error);
+    return res.status(500).json({ message: 'Error interno del servidor' });
+  }
+}
+export { isAdmin , isUser, isEmprendedor};
