b9429d969c527ed860bc77368748c4711dfc3a9a
EstebanBra <Esteban.bravo2101@alumnos.ubiobio.cl>
2024-07-07 22:33:55 -0400

    Permisos de Administrador Solucionados

--- backend/src/routes/evento.routes.js
+++ backend/src/routes/evento.routes.js
@@ -4,16 +4,16 @@ import { Router } from "express";
 // Importar funciones de controlador de eventos
 import{ crearEvento, obtenerEventos, actualizarEvento, eliminarEvento} from "../controllers/evento.controller.js"
 // Importar middleware de autenticación de administrador
-//import { isAdmin } from "../middlewares/auth.middleware.js"
+import { isAdmin } from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
 // Proteger todas las rutas con middleware de autenticación de administrador
-//router.use(isAdmin);
+router.use(isAdmin);
 
 router.post("/crearevento", crearEvento);
-router.get("/eventos", obtenerEventos);
-router.put("/eventos/:id", actualizarEvento);
-router.delete("/eventos/:id", eliminarEvento);
+router.get("/obtener", obtenerEventos);
+router.put("/update/:id", actualizarEvento);
+router.delete("/delete/:id", eliminarEvento);
 
 export default router;
